02fa265722a7570cb745a4765e2be5eb6b046019
Areeb Khan <areeb@emertech.io>
2021-11-26 20:29:04 +0530

    Fixed card match logic, where the card will now indicate that it has already been matched

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { deviceHeight, deviceWidth } from './app/common/dimensions';
 
 export default function App() {
@@ -14,6 +13,8 @@ export default function App() {
   const [matches, setMatches] = useState(0)
 
   const [matchedIndices, setMatchedIndices] = useState([])
+
+  const [isGameWon, setIsGameWon] = useState(false)
 
   const randomlySorted = (arr = placements.slice()) => {
     let insertedIndices = [] // will keep track of the occupied indices
@@ -50,66 +51,90 @@ export default function App() {
     setPlacements(randomlySorted())
   }, [])
 
+  useEffect(() => {
+    if (matchedIndices.length && (matchedIndices.length === placements.length)) setIsGameWon(true)
+  }, [matchedIndices])
+
 
   useEffect(() => {
     if (second) {
-      const doesMatch = (first === second)
+      const doesMatch = (first.value === second.value)
       if (doesMatch) setMatches(mat => ++mat)
-
       setAttempts(att => ++att)
 
       setTimeout(() => {
-        if (first === second) {
+        if (doesMatch) {
+          let _matchedIdx = matchedIndices.slice()
+          _matchedIdx.push(first.index, second.index)
+          setMatchedIndices(_matchedIdx)
           // blacken the paired cards and make them unavailable for press somehow
         }
-
         clearBoth()
       }, 500);
     }
   }, [second])
 
   const showCard = (index) => {
-    const char = placements[index]
-    if (!first) setFirst(char)
-    else setSecond(char)
-    // setAttempts(att => ++att)
+    let _selectionObj = {
+      index,
+      value: placements[index]
+    }
+    if (!first) setFirst(_selectionObj)
+    else setSecond(_selectionObj)
   }
 
+  if (isGameWon) return <SafeAreaView style={styles.container}><Text style={{ fontWeight: "bold" }}>GAME WON!</Text></SafeAreaView>
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       {/* <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" /> */}
 
       <Text>{deviceWidth} x {deviceHeight}</Text>
 
-      <Text>First Card Opened: {first}</Text>
-      <Text>Second Card Opened: {second}</Text>
+      <Text>First Card Opened: {first?.value}</Text>
+      <Text>Second Card Opened: {second?.value}</Text>
 
       <View style={styles.gridOutline}>
-        {placements.map((el, index) => <Card key={index} index={index} showCard={showCard} char={el} />)}
+        <CardsContainer
+          placements={placements}
+          showCard={showCard}
+          matchedCardIndices={matchedIndices}
+        />
       </View>
 
       <Text>Attempts: {attempts}</Text>
       <Text>Matches: {matches}</Text>
 
-    </View>
+    </SafeAreaView>
   );
 }
 
+const CardsContainer = ({ placements, showCard, matchedCardIndices }) => {
+  return (
+    <>
+      {placements.map((el, index) => <Card key={index} index={index} showCard={showCard}
+        isMatched={matchedCardIndices.includes(index)}
+        char={el} />)}
+    </>
+  )
+}
 
-const Card = ({ char, showCard, index }) => {
+
+const Card = ({ char, showCard, index, isMatched }) => {
 
   const [isPaired, setIsPaired] = useState(false)
   const [visible, setVisible] = useState(false)
 
   const cardPressHandler = () => {
+    if (isMatched) return;
     setVisible(true)
     showCard(index)
   }
 
   return (
     <Pressable style={styles.card} onPress={cardPressHandler}>
-      {visible ? <Text style={{ fontWeight: "bold" }}>{char}</Text> : <Text>(hidden)</Text>}
+      {visible ? <Text style={{ fontWeight: "bold" }}>{isMatched ? "Already matched!" : char}</Text> : <Text>(hidden)</Text>}
     </Pressable>
   )
 }
@@ -120,6 +145,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#e5e5e5',
     // width: 500,
     width: deviceWidth,
     borderWidth: 1,
@@ -138,6 +164,7 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
     borderRadius: 5,
+    backgroundColor: 'white',
 
     justifyContent: 'center',
     alignItems: 'center',
